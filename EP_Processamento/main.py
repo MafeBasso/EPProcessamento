71dfbbf8a043aa9bac2ead4cb0180f8c81563aef
Maria Fernanda Basso <mariafernandabasso@gmail.com>
2022-07-14 20:01:16 -0300

    some tests

--- EP_Processamento/main.py
+++ EP_Processamento/main.py
@@ -5,9 +5,11 @@ import os
 class Main():
     def __init__(self):
         files_path = Main.list_files(self)
+        text_file = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "results.txt"), "w")
         for file_path in files_path:
             mask, result, result_edges = Main.find_red_lines(self, file_path)
-            Main.print_images(self, mask, result, result_edges)
+            Main.print_images(self, mask, result, result_edges, file_path, text_file)
+        text_file.close()
 
     def list_files(self):
         dir_path = os.path.abspath(os.path.dirname(__file__))
@@ -34,21 +36,40 @@ class Main():
         result = image.copy()
         
         #conversão da imagem somente com as bordas de RGB para HSV
-        image_hsv = cv2.cvtColor(result_edges, cv2.COLOR_BGR2HSV)
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         
         #valores máximo e mínimo para vermelho em HSV
-        lower = np.array([140,50,20])
-        upper = np.array([180,255,255])
+        mask1 = cv2.inRange(image_hsv, (0,50,50), (10,255,255))
+        mask2 = cv2.inRange(image_hsv, (145,30,50), (180,255,255))
+
+        ## Merge the mask and crop the red regions
+        mask = cv2.bitwise_or(mask1, mask2)
+        # lower = np.array([100,50,20])
+        # upper = np.array([180,255,255])
         
         #máscara para selecionar as partes da imagem que estão entre esses valores
-        mask = cv2.inRange(image_hsv, lower, upper)
+        # mask = cv2.inRange(image_hsv, lower, upper)
+        # tessst = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5)), iterations = 3)
         
         #retira da imagem original tudo aquilo que não está na máscara
         result = cv2.bitwise_and(result, result, mask=mask)
 
-        return mask, result, result_edges
+        return mask, result, image
 
-    def print_images(self, mask, result, result_edges):
+    def print_images(self, mask, result, result_edges, file_path, text_file):
+        #Utilizando algorítimo para reconhecer retangulos na imagem
+        print(file_path + ":")
+        cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        text_file.write(file_path + "\n")
+        for cnt in cnts:
+            approx = cv2.contourArea(cnt)
+            if (5.0 < approx < 1500):
+                x,y,w,h = cv2.boundingRect(cnt)
+                cv2.rectangle(result,(x,y),(x+w,y+h),(0,255,0),2)
+                print(approx)
+                text_file.write(str(approx) + " ")
+        text_file.write("\n")
         #configuração da janela para aparecer na tela
         cv2.namedWindow( "mask", cv2.WINDOW_NORMAL)
         cv2.namedWindow( "result", cv2.WINDOW_NORMAL)
